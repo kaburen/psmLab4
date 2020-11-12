5374672e8f5a7baeeb357ac4371f9ebec0a18f0c
unknown <andrzejosika06@gmail.com>
2020-11-12 15:09:19 +0100

    refactored operatios to utils

--- App.js
+++ App.js
@@ -1,46 +1,78 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import Button from "./components/button";
-export default function App() {
-    return (
-        <View style={styles.container}>
-            <View style={styles.resultContainer}>
-                <Text style={styles.resultTexT}>0</Text>
+import calculate from "./utils/calcOperations";
+import {factorial} from 'mathjs';
+
+export default class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            result: 0,
+            part: '',
+            operation: '',
+        };
+    }
+
+    handleKeys = (operation, value) => {
+        this.setState((state) => calculate(operation, value, state))
+    }
+
+    render() {
+        const {result} = this.state;
+        return (
+            <View style={styles.container}>
+                <View style={styles.resultContainer}>
+                    <Text style={styles.resultTexT}>{result}</Text>
+                </View>
+
+                <View style={styles.buttonsContainer}>
+                    <View style={styles.buttonsRow}>
+                        <Button isHidden extraButton fun={() => this.handleKeys("land",'pi')}>π</Button>
+                        <Button isHidden extraButton fun={() => this.handleKeys("land",'x3')}>x³</Button>
+                        <Button doubleButton fun={() => this.handleKeys("digit", 0)}>0</Button>
+                        <Button fun={() => this.handleKeys("comma")}>,</Button>
+                        <Button actionButton fun={() => this.handleKeys("port")}>=</Button>
+                    </View>
+                    <View style={styles.buttonsRow}>
+                        <Button isHidden extraButton fun={() => this.handleKeys("land",'e')}>e</Button>
+                        <Button isHidden extraButton fun={() => this.handleKeys("land",'x2')}>x²</Button>
+                        <Button fun={() => this.handleKeys("digit", 1)}>1</Button>
+                        <Button fun={() => this.handleKeys("digit", 2)}>2</Button>
+                        <Button fun={() => this.handleKeys("digit", 3)}>3</Button>
+                        <Button actionButton fun={() => this.handleKeys("operator",'+')}>+</Button>
+                    </View>
+                    <View style={styles.buttonsRow}>
+                        <Button isHidden extraButton fun={() => this.handleKeys("land",'ln')}>ln</Button>
+                        <Button isHidden extraButton fun={() => this.handleKeys("land",'log10')}>log10</Button>
+                        <Button fun={() => this.handleKeys("digit", 4)}>4</Button>
+                        <Button fun={() => this.handleKeys("digit", 5)}>5</Button>
+                        <Button fun={() => this.handleKeys("digit", 6)}>6</Button>
+                        <Button actionButton fun={() => this.handleKeys("operator",'-')}>-</Button>
+                    </View>
+                    <View style={styles.buttonsRow}>
+                        <Button isHidden extraButton fun={() => this.handleKeys("land",'ex')}>e×</Button>
+                        <Button isHidden extraButton fun={() => this.handleKeys("land",'10x')}>10×</Button>
+                        <Button fun={() => this.handleKeys("digit", 7)}>7</Button>
+                        <Button fun={() => this.handleKeys("digit", 8)}>8</Button>
+                        <Button fun={() => this.handleKeys("digit", 9)}>9</Button>
+                        <Button actionButton fun={() => this.handleKeys("operator",'*')}>x</Button>
+                    </View>
+                    <View style={styles.buttonsRow}>
+                        <Button isHidden extraButton fun={() => this.handleKeys("operator",'sqrt')}>x√y</Button>
+                        <Button isHidden extraButton fun={() => this.handleKeys( "land",'x!')}>x!</Button>
+                        <Button fun={() => this.handleKeys("AC")}>AC</Button>
+                        <Button isHidden fun={() => this.handleKeys("sign")}>+/-</Button>
+                        <Button isHidden fun={() => this.handleKeys("land",'%')}>%</Button>
+                        <Button isDisabled doubleButton> </Button>
+                        <Button actionButton fun={() => this.handleKeys("operator",'/')}>÷</Button>
+                    </View>
+                </View>
+
             </View>
 
-            <View style={styles.buttonsContainer}>
-                <View style={styles.buttonsRow}>
-                    <Button doubleButton>0</Button>
-                    <Button>,</Button>
-                    <Button actionButton actionText>=</Button>
-                </View>
-                <View style={styles.buttonsRow}>
-                    <Button>1</Button>
-                    <Button>2</Button>
-                    <Button>3</Button>
-                    <Button actionButton actionText>+</Button>
-                </View>
-                <View style={styles.buttonsRow}><Button>4</Button>
-                    <Button>5</Button>
-                    <Button>6</Button>
-                    <Button actionButton actionText>-</Button>
-                </View>
-                <View style={styles.buttonsRow}>
-                    <Button>7</Button>
-                    <Button>8</Button>
-                    <Button>9</Button>
-                    <Button actionButton actionText>x</Button>
-                </View>
-                <View style={styles.buttonsRow}>
-                    <Button>AC</Button>
-                    <Button isDisabled doubleButton> </Button>
-                    <Button actionButton actionText>/</Button>
-                </View>
-            </View>
-
-        </View>
-
-    );
+        );
+    }
 }
 
 const styles = StyleSheet.create({
@@ -59,7 +91,7 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-end',
     },
     resultTexT: {
-        fontSize: 84,
+        fontSize: 64,
         color: 'white',
         paddingHorizontal: 10,
     },
